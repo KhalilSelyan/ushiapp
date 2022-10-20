81e3901419b1ed86d0adfd260d89ec30f1ff24a7
Khalil Selyan <khalilselyan2@gmail.com>
2022-10-21 00:22:48 +0300

    not fully working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Socket } from "socket.io";
 import { io } from "socket.io-client";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
+import { v4 as uuidv4 } from "uuid";
 
 function Test() {
   const [url, setUrl] = React.useState([""]);
@@ -19,7 +20,11 @@ function Test() {
 
   const [currentTime, setCurrentTime] = React.useState(0);
   const [isPaused, setIsPaused] = React.useState(sourceRef.current?.paused);
-  const [roomNumber, setRoomNumber] = React.useState(0);
+  const [roomNumber, setRoomNumber] = React.useState("0");
+  const virtualId = uuidv4();
+
+  const [hostMessages, setHostMessages] = React.useState<string[]>([]);
+  const [guestMessages, setGuestMessages] = React.useState<string[]>([]);
 
   useEffect(() => {
     const ss:
@@ -34,17 +39,25 @@ function Test() {
     } else setIsPaused(false);
   }, [sourceRef.current?.paused]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (socket) {
       type === "host"
         ? socket.emit("host", {
             currentTime: currentTime,
             isPaused: isPaused,
             roomNumber: roomNumber,
+            url: url,
+            type: type,
+            // hostMessages: hostMessages,
+            // guestMessages: guestMessages,
           })
         : socket.on("watcher", (data) => {
-            console.log("watcher", data);
             if (data.roomNumber === roomNumber) {
+              console.log("data", data);
+              const newUrl = [data.url[data.url.length - 1]];
+              if (newUrl[newUrl.length - 1] !== url[url.length - 1]) {
+                setUrl(newUrl);
+              }
               if (sourceRef.current) {
                 if (data.isPaused) {
                   sourceRef.current.pause();
@@ -61,9 +74,25 @@ function Test() {
             }
           });
     }
-  }, [currentTime, isPaused]);
+  }, [currentTime, guestMessages, hostMessages, isPaused]);
 
-  if (!type && roomNumber === 0) {
+  if (!type) {
+    return (
+      <div className="flex h-screen w-screen items-center justify-center gap-x-2">
+        <button
+          onClick={() => {
+            setType("host");
+            setRoomNumber(virtualId);
+          }}
+        >
+          Host
+        </button>
+        <button onClick={() => setType("guest")}>Guest</button>
+      </div>
+    );
+  }
+
+  if (type === "guest" && roomNumber === "0") {
     return (
       <div className="flex h-screen w-full items-center justify-center space-x-2 bg-sky-400">
         <span>Input room Number: </span>
@@ -73,85 +102,81 @@ function Test() {
           className="rounded-md border-2 border-black p-2"
           onChange={(e) => {
             setTimeout(() => {
-              setRoomNumber(Number(e.target.value));
-            }, 2000);
+              setRoomNumber(e.target.value);
+            }, 1000);
           }}
         />
       </div>
     );
   }
 
-  if (!type && roomNumber !== 0) {
+  if (roomNumber !== "0")
     return (
-      <div className="flex h-screen w-screen items-center justify-center gap-x-2">
-        <button onClick={() => setType("host")}>Host</button>
-        <button onClick={() => setType("guest")}>Guest</button>
-      </div>
-    );
-  }
+      <div className="flex h-screen w-full flex-col items-center overflow-scroll bg-blue-200 p-4">
+        {/* input for user to enter the url */}
+        <div className="flex w-full items-center justify-center space-x-2">
+          <span>Url:</span>
+          <input
+            type="text"
+            className="h-10 w-1/2 border-2 border-black"
+            onChange={(event) => {
+              // wait for user to finish typing
+              const value = event.target.value as string;
+              setTimeout(() => setUrl([...url, value]), 1000);
+            }}
+          />
+        </div>
 
-  return (
-    <div className="flex h-screen w-full flex-col items-center overflow-scroll bg-blue-200 p-4">
-      {/* input for user to enter the url */}
-      <div className="flex w-full items-center justify-center space-x-2">
-        <span>Url:</span>
-        <input
-          type="text"
-          className="h-10 w-1/2 border-2 border-black"
-          onChange={(event) => {
-            // wait for user to finish typing
-            const value = event.target.value as string;
-            setTimeout(() => setUrl([...url, value]), 1000);
-          }}
-        />
-      </div>
-
-      {/* video player only render the last added url*/}
-      {url.map(
-        (item, index) =>
-          url.length - 1 === index && (
-            <div
-              key={index}
-              className="flex w-full flex-1 items-center justify-center"
-            >
-              <video
+        {/* video player only render the last added url*/}
+        {url.map(
+          (item, index) =>
+            url.length - 1 === index && (
+              <div
                 key={index}
-                ref={sourceRef}
-                className="h-4/5 w-3/5 rounded-md border-2 border-black"
-                controls
-                controlsList="download"
-                autoPlay
-                onCanPlay={() => {
-                  // get the video element
-                  const video = sourceRef.current;
-                  // get the video duration
-                  const duration = video?.duration;
-
-                  console.log(duration);
-
-                  // get the video current time
-                  setInterval(() => {
-                    setCurrentTime(sourceRef.current?.currentTime as number);
-                  }, 500);
-                }}
+                className="flex w-full flex-1 items-center justify-center"
               >
-                <source src={item} type="video/mp4" />
-              </video>
+                <video
+                  key={index}
+                  ref={sourceRef}
+                  className="h-4/5 w-3/5 rounded-md border-2 border-black"
+                  controls
+                  controlsList="download"
+                  autoPlay
+                  onCanPlay={() => {
+                    // get the video current time
+                    setInterval(() => {
+                      setCurrentTime(sourceRef.current?.currentTime as number);
+                    }, 500);
+                  }}
+                >
+                  <source src={item} type="video/mp4" />
+                </video>
+              </div>
+            )
+        )}
+        <div className="absolute top-0 right-0 z-10 h-screen w-72 min-w-max bg-black text-white">
+          <div className="space-between flex flex-col space-y-2">
+            <div className="flex flex-col items-center justify-center space-y-2">
+              <span>Room Number: {roomNumber}</span>
+              <span>Current Time: {currentTime}</span>
+              <span>Is Paused: {isPaused ? "true" : "false"}</span>
             </div>
-          )
-      )}
-      <div className="absolute top-0 right-0 z-10 h-screen w-72 min-w-max bg-black text-white">
-        <div className="flex flex-col  space-y-2">
-          <div className="flex flex-col items-center justify-center space-y-2">
-            <span>Room Number: {roomNumber}</span>
-            <span>Current Time: {currentTime}</span>
-            <span>Is Paused: {isPaused ? "true" : "false"}</span>
+            <div className="flex flex-col items-start justify-start space-y-2 p-2">
+              <span>Host Messages:</span>
+              {hostMessages.map((item, index) => (
+                <span key={index}>{item}</span>
+              ))}
+              <div className="flex ">
+                <span>Guest Messages:</span>
+                {guestMessages.map((item, index) => (
+                  <span key={index}>{item}</span>
+                ))}
+              </div>
+            </div>
           </div>
-          <div className="flex flex-col items-start justify-start space-y-2 p-2"></div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default Test;
